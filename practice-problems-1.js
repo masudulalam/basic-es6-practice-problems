489532ab4140963f7dbcb107fedb22911555b28c
masudulalam <masuudulalam@gmail.com>
2024-04-20 08:23:45 +0600

    problem 3 has solved

--- practice-problems-1.js
+++ practice-problems-1.js
@@ -1,8 +1,8 @@
 // 1. Write an arrow function that will take 3 parameters, will multiply the parameters and will return the result.
 
 const multiply = (n1, n2, n3) => n1 * n2 * n3;
-const result = multiply(4, 5, 6,);
-console.log(result);
+// const result = multiply(4, 5, 6,);
+// console.log(result);
 
 /* 2. Write the following sentence in three lines and print the result: 
 I am a web developer. I love to code. I love to eat fruits. */
@@ -11,7 +11,15 @@ const aboutMe = `I am a web developer.
 I love to code. 
 I love to eat fruits.`;
 
-console.log(aboutMe);
+// console.log(aboutMe);
+
+
+// Write a arrow function that will take two parameters: one parameter will come from you and other parameter will be a default parameter. Add these two parameters and return the result.
+
+const sum = (a, b = 1) => a + b;
+
+const result = sum (40);
+console.log(result);
 
 
 
