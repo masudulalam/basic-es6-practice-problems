fdc456a246333da1bd3e660d9086dc268840e2b4
masudulalam <masuudulalam@gmail.com>
2024-04-20 06:03:56 +0600

    problem no. 1 is solved

---
+++ practice-problems-1.js
@@ -0,0 +1,5 @@
+// 1. Write an arrow function that will take 3 parameters, will multiply the parameters and will return the result.
+
+const multiply = (n1, n2, n3) => n1 * n2 * n3;
+const result = multiply(4, 5, 6,);
+console.log(result);
