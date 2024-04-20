689f89928396a7cac0b045e1c5a71b7acbc85572
masudulalam <masuudulalam@gmail.com>
2024-04-20 06:13:51 +0600

    problem 2 soloved

--- practice-problems-1.js
+++ practice-problems-1.js
@@ -3,3 +3,15 @@
 const multiply = (n1, n2, n3) => n1 * n2 * n3;
 const result = multiply(4, 5, 6,);
 console.log(result);
+
+/* 2. Write the following sentence in three lines and print the result: 
+I am a web developer. I love to code. I love to eat fruits. */
+
+const aboutMe = `I am a web developer. 
+I love to code. 
+I love to eat fruits.`;
+
+console.log(aboutMe);
+
+
+
